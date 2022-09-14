84d35bfea2e35a5cfeb2b39be3ef98483b2c29c3
shz-cod <web.shahidul.alam@gmail.com>
2022-09-14 23:15:09 +0600

    v1.1 (Like functionality)

--- script.js
+++ script.js
@@ -35,4 +35,27 @@ else{
 }
 
 
+// Like Function 
+const likeBtn = document.querySelectorAll(".like-btn");
+
+likeBtn.forEach(btn => {
+	btn.addEventListener("click" , (e) => {
+		let CountDisplay = e.target.parentNode.querySelector("span");
+		let count = CountDisplay.innerHTML;
+		count = parseInt(count,10);
+		if(e.target.classList.contains("fa-regular"))
+		{
+			e.target.classList.remove("fa-regular");
+			e.target.classList.add("fa-solid");
+			CountDisplay.innerHTML = `${count + 1}`;
+		}
+		else{
+			e.target.classList.add("fa-regular");
+			e.target.classList.remove("fa-solid");
+			CountDisplay.innerHTML = `${count - 1}`;
+		}
+	})
+})
+
+
 
